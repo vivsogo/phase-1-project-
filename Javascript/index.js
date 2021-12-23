2e268dcea4cdbf98d51e2d197bc542120e1cc502
Vivian Gomez <viviangomez0@gmail.com>
2021-12-23 16:14:41 -0500

    added images 2/25

---
+++ Javascript/index.js
@@ -0,0 +1,9 @@
+
+//the carousel allows the users to "swipe" through images 
+// Get back to this pending idea 
+$(document).ready(function(){
+    $('.slider').slider();
+  });
+//
+
+alert("great pick!")
